106f2eec78a14dc2ebb454e81b273e4c2e4cc738
ozankaze <ozankaze101@gmail.com>
2018-07-12 08:35:22 +0700

    read latihan

--- view/header.php
+++ view/header.php
@@ -9,17 +9,20 @@
 </head>
     <body class="container">
         <header>
-            <h1>Daftar Nama Siswa QODR</h1>
+            <h1>Belajar apa yang di pelajari ...</h1>
         </header>
 
         <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-            <a class="navbar-brand" href="index.php">Daftar Nama</a>
+            <a class="navbar-brand" href="#">QODR</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
 
             <div class="collapse navbar-collapse" id="navbarColor01">
                 <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php">Nama Siswa</a>
+                    </li>
                     <li class="nav-item active">
                         <a class="nav-link" href="register.php">Register <span class="sr-only">(current)</span></a>
                     </li>
