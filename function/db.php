4009cc34002155b0adb83277d8e58950879e42db
ozankaze <ozankaze101@gmail.com>
2018-07-25 22:04:33 +0700

    DB

--- function/db.php
+++ function/db.php
@@ -3,7 +3,7 @@
 $host = 'localhost';
 $name = 'root';
 $password = '123';
-$db = 'php_siswa_sekolakoding';
+$db = 'php_siswa_crud_sekolakoding';
 
 $link = mysqli_connect($host, $name, $password, $db) or die(mysqli_error());
 
